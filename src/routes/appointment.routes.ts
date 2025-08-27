dab9f585dab25050fce7f75dddf4d08943624b27
CaioVictor <caiorbcavalcante@gmail.com>
2025-08-26 21:28:38 -0300

    Adiconado mais rotas do agendamentos

--- src/routes/appointment.routes.ts
+++ src/routes/appointment.routes.ts
@@ -6,3 +6,7 @@ export const appointmentRouter = Router()
 const appointmentController = new AppointmentController()
 
 appointmentRouter.post("/appointments", appointmentController.createAppointment)
+appointmentRouter.get("/appoinments", appointmentController.getAllAppointments) // Listar todos os agendamentos
+appointmentRouter.get("/appointments", appointmentController.getAppointment) // Listar appointment específico através do id
+appointmentRouter.put("/appointments:id", appointmentController.createAppointment)
+appointmentRouter.delete("/appointments:id", appointmentController.deleteAppointment)
