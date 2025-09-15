145026c1fdb6b00e3db9cac6f18e7f5421ad48eb
Leonardo Araujo <leonardoaraujo100006@gmail.com>
2025-09-15 17:29:13 -0300

    criando o userProfile.tsx

--- frontend/app/src/pages/userPage/UserPage.tsx
+++ frontend/app/src/pages/userPage/UserPage.tsx
@@ -3,18 +3,5 @@ import { useState } from "react"
 
 
 export const UserPage:React.FC = () => {
-    const [user,setUser] = useState<any>(null)
-    const [appointments, setAppointments] = useState([]);
-    const [services, setServices] = useState([]);
-
-    const token = localStorage.getItem("token")
-    const id_user = localStorage.getItem("id_user")
-
-    useEffect(() => {
-        
-    })
-
-    return(
-        
-    )
+    
 }
