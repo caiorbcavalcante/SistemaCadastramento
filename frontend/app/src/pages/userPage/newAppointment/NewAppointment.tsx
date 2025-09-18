e82a964be122db15d447e9bd46a310ddc9aed3cc
Leonardo Araujo <leonardoaraujo100006@gmail.com>
2025-09-18 18:14:37 -0300

    userPage finalizada

--- frontend/app/src/pages/userPage/newAppointment/NewAppointment.tsx
+++ frontend/app/src/pages/userPage/newAppointment/NewAppointment.tsx
@@ -1,15 +1,26 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+
+interface IBarber {
+  id_barber: string;
+  name: string;
+}
+
+interface IService {
+  id_service: string;
+  description: string;
+  price: number;
+}
+
 export const NewAppointment: React.FC = () => {
   const [barber, setBarber] = useState("")
   const [service, setService] = useState("")
   const [date, setDate] = useState("")
   const [error, setError] = useState<string | null>(null)
-  const [success,setSuccess] = useState("")
-
-  const [barbers, setBarbers] = useState([]);
-  const [services, setServices] = useState([]);
+  const [success,setSuccess] = useState<string | null>(null)
+  const [barbers, setBarbers] = useState<IBarber[]>([]);
+  const [services, setServices] = useState<IService[]>([]);
 
   const token = localStorage.getItem("token");
   const id_user = localStorage.getItem("id_user");
@@ -43,9 +54,82 @@ export const NewAppointment: React.FC = () => {
 },[token])
 
 
-    const handleCreate
+    const handleCreate = async () => {
+      setError(null)
+      setSuccess(null)
 
+      if(!barber || !service || !date) {
+        setError("Preencha todos os campos.");
+        return
+      }
+
+      try{
+         if (!token || !id_user) throw new Error("Usuário não autenticado.")
+
+           await axios.post("http://localhost:3000/appointments",
+            {
+              user:id_user,
+              barber:barber,
+              service:service,
+              date
+
+            }, {
+               headers: { Authorization: `Bearer ${token}` },
+            }
+          )
+          
+        setSuccess("Agendamento realizado com sucesso!")
+        setBarber("")
+        setService("")
+        setDate("")
+
+      }catch{
+        setError("Erro ao criar agendamento. Tente novamente.")
+      }
+    }
+
+    const handleClear = () => {
+        setBarber("")
+        setService("")
+        setDate("")
+        setError(null)
+        setSuccess(null)
+    }
     return(
+      <div>
+        <h2>Agendar Serviço</h2>
+
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        {success &&  <p style={{ color: "green" }}>{success}</p>}
+
+        <select onChange={(e) => setBarber(e.target.value)} value={barber}>
+          <option value="">Selecione o Barbeiro</option>
+          {barbers.map((b)=> (
+            <option key={b.id_barber} value={b.id_barber}> 
+            {b.name}
+             </option>
+          ))}
+        </select>
+
+        <select onChange={(e)=> setService(e.target.value)} value={service}>
+          <option>Selecione o Serviço</option>
+          {services.map((s)=>(
+            <option key={s.id_service} value={s.id_service}>
+              {s.description} - R$ {s.price.toFixed(2)}
+            </option>
+          ))}
+
+        </select>
+
+        <input placeholder=" Data do Agendamento"  type="datetime-local" value={date} onChange={(e)=> setDate(e.target.value)}/>
+
+
+        <div style={{ marginTop: "1rem" }}>
+          <button onClick={handleCreate} style={{ marginRight: "1rem" }}>Agendar</button>
+
+          <button onClick={handleClear}>Cancelar</button>
+          </div>
+      </div>
 
     )
 
