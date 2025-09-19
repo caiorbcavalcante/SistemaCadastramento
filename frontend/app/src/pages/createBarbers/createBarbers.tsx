14abd46c98b152da499f6926d0e66fe3a5362e22
CaioVictor <caiorbcavalcante@gmail.com>
2025-09-18 21:05:21 -0300

    feat

---
+++ frontend/app/src/pages/createBarbers/createBarbers.tsx
@@ -0,0 +1,62 @@
+import './Form.css' // Pode criar um CSS genérico para os formulários
+import { useState } from 'react'
+import axios from "axios"
+
+const createBarbers: React.FC = () => {
+    const [name, setName] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = user<string>("");
+
+    const isValidEmail = (email: string): boolean => {
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return regex.test(email);
+    }
+    const createBarber = async (e: React.FormEvent) => {
+        e.preventDefault()
+
+        try {
+            if (!isValidEmail(email)){
+                alert("Email inválido!");
+                return;
+            }
+
+            const response = await axios.post("http://localhost:3000/barbers",{ // Enviando o barbeiro criado para o back-end
+                name,
+                email,
+                password
+            });
+
+            console.log("Barbeiro criado")
+        } catch (error: any) {
+            console.log("Erro ao criar barbeiro", error);
+        }
+    }
+
+    return (
+        <form onSubmit={createBarber}>
+            <input 
+                type="text" 
+                placeholder="Nome" 
+                value={name} 
+                onChange={(e) => setName(e.target.value)} 
+            />
+            <input 
+                type="email" 
+                placeholder="Email" 
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)} 
+            />
+            <input 
+                type="password" 
+                placeholder="Senha" 
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)} 
+            />
+            <button type="submit">Criar Barbeiro</button>
+        </form>
+    );
+
+}
+
+
+
