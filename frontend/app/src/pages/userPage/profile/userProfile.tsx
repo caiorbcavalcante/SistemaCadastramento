4d107c05d2df210f142e742f3d52690a1718e868
Leonardo Araujo <leonardoaraujo100006@gmail.com>
2025-09-19 12:29:02 -0300

    vtmnc

--- frontend/app/src/pages/userPage/profile/userProfile.tsx
+++ frontend/app/src/pages/userPage/profile/userProfile.tsx
@@ -3,15 +3,39 @@ import React, { useEffect, useState } from "react";
 import { Input } from "../../../components/input/Input";
 import { Button } from "../../../components/button/Button";
 
+
+const validadeForm = (name: string, password: string, email:string, number : number): string | null => {
+     if (!name || !password ||!email ||!number) {
+     return "Preencha todos os campos."
+    }
+
+    if (!email.includes("@") || !email.includes(".")) {return "Email inválido."}
+    if (password.length < 6) { return "A senha deve ter pelo menos 6 caracteres."}
+    const digitsOnly = number.replace(/\D/g, ""); // remove tudo que não é número
+  if (digitsOnly.length !== 11) return "O número deve ter 11 dígitos";
+    return null;
+}
+
+const formatPhoneNumber = (value: string) => {
+  // Remove tudo que não é número
+  const digits = value.replace(/\D/g, "");
+
+  // Formata conforme o tamanho
+  if (digits.length <= 2) return digits; // só DDD
+  if (digits.length <= 7) return `${digits.slice(0, 2)} ${digits.slice(2)}`; // DDD + parte inicial
+  return `${digits.slice(0, 2)} ${digits.slice(2, 7)}-${digits.slice(7, 11)}`}
+
 interface User {
   id: number;
   name: string;
-  password: string;
+  password?: string;
+  email:string
+  number:string
 }
 export const UserProfile: React.FC = () => {
   const [user, setUser] = useState<User | null>(null);
   const [edit, setEdit] = useState(false);
-  const [form, setForm] = useState({ name: "", password: "" });
+  const [form, setForm] = useState({ name: "", password: "",email:"",number:""});
   const [error, setError] = useState<string | null>(null);
 
   const token = localStorage.getItem("token");
@@ -27,7 +51,7 @@ export const UserProfile: React.FC = () => {
         });
 
         setUser(res.data);
-        setForm({ name: res.data.name, password: "" })
+        setForm({ name: res.data.name, password: "", email:res.data.email, number: res.data.number })
         setError(null);
       } catch {
         setError("Erro ao buscar usuário");
@@ -40,19 +64,21 @@ export const UserProfile: React.FC = () => {
   const handleSave = async () => {
     setError(null);
 
-    if (!form.name || !form.password) {
-      setError("Preencha todos os campos.");
-      return;
+    const validationError = validadeForm(form.name, form.email, form.password, form.number)
+    if(validationError){
+      setError(validationError)
+      return
     }
 
     try {
       const res = await axios.patch(
         `http://localhost:3000/user/${id_user}`,
-        form,
+        { name: form.name, password: form.password, number: Number(form.number) },
         { headers: { Authorization: `Bearer ${token}` } }
       );
       setUser(res.data);
       setEdit(false);
+      setForm({ ...form, password: "" })
       setError(null);
     } catch {
       setError("Erro ao atualizar perfil. Tente novamente.");
@@ -78,10 +104,22 @@ export const UserProfile: React.FC = () => {
         value={form.password}
         onChange={(e) => setForm({... form, password:e.target.value})}/> 
 
+        <Input
+        placeholder="email"
+        value={form.email}
+        onChange={(e) =>  setForm({... form, email:e.target.value})}/>
+
+        <Input
+          placeholder="Número (DDD + telefone)"
+          type="number"
+          value={form.number}
+          onChange={(e) => setForm({ ...form, number: formatPhoneNumber(e.target.value) })}
+          />
+
         <Button text="Salvar" onClick={handleSave} />
 
         <Button text="Cancelar" onClick={() => {
-        setForm({ name: user.name, password: user.password });
+        setForm({ name: user.name, password: "", email: user.email, number: user.number });
         setEdit(false);
         setError(null);
         }} />
@@ -90,7 +128,8 @@ export const UserProfile: React.FC = () => {
     ):(
       <div> 
         <p>Nome:{user.name}</p>
-        <p>password:{user.password}</p>
+        <p>Email:{user.email}</p>
+        <p>Número: {user.number}</p>
 
         <Button text="Editar Perfil" onClick={() => setEdit(true)} />
       </div>
