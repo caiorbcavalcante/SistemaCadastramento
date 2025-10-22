0d523d8a697712ed533151cc3031728682906243
Leonardo Araujo <leonardoaraujo100006@gmail.com>
2025-10-22 15:07:35 -0300

    oisao

--- frontend/app/vite.config.ts
+++ frontend/app/vite.config.ts
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: '_redirects',
-          dest: '.' // copia o arquivo para a pasta dist/
-        }
-      ]
-    })
-  ],
+  plugins: [react()],
 })
